36385d21291183fb51c185c95eab52f29fb029d8
Varaprasad Kasturi <vk67220@corp.cyient.com>
2026-01-29 18:46:45 +0530

    added

--- src/main/java/net/javaguides/springboot/service/EmployeeService.java
+++ src/main/java/net/javaguides/springboot/service/EmployeeService.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import net.javaguides.springboot.model.Employee;
 
 //list of employees details
+//service interface
 
 public interface EmployeeService {
 	List<Employee> getAllEmployees();
